6e6b97f3935a9c3cdcfd6c870ba189b426442f34
Joe Mac <jhmccarney@gmail.com>
2024-03-15 11:50:18 +0000

    Implemented client-side sort functionality

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import FeaturedPosts from '../FeaturedPosts/FeaturedPosts'
 import ArticleGrid from '../ArticleList/ArticleGrid'
 import { getAllArticles } from '../../utils/api'
 import Loading from '../../utils/Loading'
@@ -9,24 +8,41 @@ import {useParams} from 'react-router-dom'
 const Main = () => {
   const [loading, setLoading] = useState(true)
   const [articles, setArticles] = useState([])
+  const [sort, setSort] = useState('date,desc')
   const {topic} = useParams()
 
+  
   useEffect(() =>{
     getAllArticles().then(res =>{
-      const data = res.data.articles
+      let data = res.data.articles
       if(topic){
-        const filteredData = data.filter(article => article.topic === topic)
-        setArticles(filteredData)
-      } else {
-        setArticles(data)
+        data = data.filter(article => article.topic === topic)
       }
+      data.sort((a,b) =>{
+        const [key, order] = sort.split(',')
+        if(order === 'desc'){
+          return b[key] - a[key]
+        } else {
+          return a[key] - b[key]
+        }
+      })
+      setArticles(data)
       setLoading(false)
     })
-  }, [topic])
-
+  }, [topic, sort])
+  const handleSortChange = (e) => {
+    setSort(e.target.value)
+ }
   return (
-    <div>
-
+    <div className='main-container'>
+        <select className='sort-select' onChange={handleSortChange} placeholder='Sort by'>
+        <option value="date,desc" defaultChecked>Date (Newest)</option>
+        <option value="date,asc">Date (Oldest)</option>
+        <option value="comment_count,desc">Comment Count (High to Low)</option>
+        <option value="comment_count,asc">Comment Count (Low to High)</option>
+        <option value="votes,desc">Votes (High to Low)</option>
+        <option value="votes,asc">Votes (Low to High)</option>
+      </select>
     
         {loading ? <Loading /> : <ArticleGrid className='article-grid' articles={articles}/>}
         
